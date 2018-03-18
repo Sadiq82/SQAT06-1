c13f0bcfaef324c0051ad77feec29679d0c0247d
sadiq ali <sadiqali@hotmail.ca>
2018-03-18 00:12:01 -0400

    sadiqbaba

--- features/scenario.feature
+++ features/scenario.feature
@@ -3,14 +3,13 @@ Feature: Simple maths
   As a developer
   I want to increment variables
 
-  Scenario: easy maths
-    Given I go to duck duck go
-    When  I search for WebdriverIO
-    Then  I should see the search results
+      Scenario: figure2
+    Given I go to Figure2 registration page
+    When  I enter create to account
+    Then  I should see the result for sign up
 
-
-Scenario: easy maths
-    Given I go to google
-    When  I search for WebdriverIO on google
-    Then  I should see the search results on google
+    Scenario: figure1
+    Given I go to Figure1 registration page
+    When  I enter create account
+    Then  I should see the result for sign up error
 
--- features/support/steps.js
+++ features/support/steps.js
@@ -3,54 +3,83 @@ const chai = require('chai')
 const should = chai.should()
 
 "use strict";
-Given('I go to duck duck go', function(callback) {
-  this.browser
-    .init()
-    .url('https://duckduckgo.com/').then(function() {
-      callback();
-    })
-})
 
-When('I search for WebdriverIO', function(callback) {
+Given('I go to Figure2 registration page',function(callback) {
   this.browser
-    .setValue('#search_form_input_homepage', 'WebdriverIO')
-    .click('#search_button_homepage').then(function(){
-      callback();
-    })
-})
-
-Then('I should see the search results', function(callback) {
-  this.browser
-    .getTitle().then(function(result){
-        result.should.equal("WebdriverIO at DuckDuckGo");
-        callback();
+  .init()
+  .url('https://app.figure1.com/account/register').then(function()
+  {callback();
   }).catch(function(error){
     callback(error);
   })
 })
 
-Given('I go to google', function(callback) {
+When('I enter create to account', function(callback){
   this.browser
-    .init()
-    .url('https://google.com/').then(function() {
-      callback();
-    })
-})
-
-When('I search for WebdriverIO on google', function(callback) {
-  this.browser
-    .setValue('#lst-ib', 'WebdriverIO')
-    .keys('Enter').then(function(){
-      callback();
-    })
-})
-
-Then('I should see the search results on google', function(callback) {
-  this.browser
-    .getTitle().then(function(result){
-        result.should.equal("WebdriverIO - Google Search");
-        callback();
+  .waitForVisible('.register-page__username-input')
+  .setValue('.register-page__username-input', this.randomUsers())
+  .setValue('.register-page__email-input',this.randomUsers()+'sadiqbaba@yahoo.com')
+  .setValue('.register-page__password-input','sadiq1')
+  .setValue('.register-page__confirm-password-input','sadiq1')
+  .addValue('.register-page__specialties-list','Doctor')
+  .waitForVisible('.register-page__specialties-other-list')
+  .addValue('.register-page__specialties-other-list','Doctor Resident')
+  .click('.box')
+  .click('.register-page__submit-button')
+    .then(function(){
+    callback();
   }).catch(function(error){
     callback(error);
   })
+})
+
+Then('I should see the result for sign up', function(callback) {
+  this.browser
+  .waitForVisible('.home-feed__share-case', 5000)
+  .getText('.home-feed__share-case').then(function(result){
+    result.should.equal("Have an interesting case to share?");
+    callback();
+}).catch(function(error){
+callback(error);
+})
+})
+
+Given('I go to Figure1 registration page',function(callback) {
+  this.browser
+  .init()
+  .url('https://app.figure1.com/account/register').then(function()
+  {callback();
+  }).catch(function(error){
+    callback(error);
+  })
+})
+
+When('I enter create account', function(callback){
+  this.browser
+  .waitForVisible('.register-page__username-input')
+  .setValue('.register-page__username-input','sadiq')
+  .setValue('.register-page__email-input','sadiqbibaba@yahoo.com')
+  .setValue('.register-page__password-input','sadiq1')
+  .setValue('.register-page__confirm-password-input','sadiq1')
+  .addValue('.register-page__specialties-list','Doctor')
+  .waitForVisible('.register-page__specialties-other-list')
+  .addValue('.register-page__specialties-other-list','Doctor Resident')
+  .click('.box')
+  .click('.register-page__submit-button')
+    .then(function(){
+    callback();
+  }).catch(function(error){
+    callback(error);
+  })
+})
+
+Then('I should see the result for sign up error', function(callback) {
+  this.browser
+  .waitForVisible('.error-message-light')
+  .getText('.error-message-light').then(function(result){
+    result.should.equal("An account with that username already exists.");
+    callback();
+}).catch(function(error){
+callback(error);
+})
 })
